cba2bbd6406f3038664bc155ec3ecffbccc8dfd7
CampbellKirkJoshua <jcampb32@uncc.edu>
2021-03-24 17:49:16 -0400

    test commit 2
    test commit 2

--- Stack Bar Chart Olympics.py
+++ Stack Bar Chart Olympics.py
@@ -2,27 +2,25 @@ import pandas as pd
 import plotly.offline as pyo
 import plotly.graph_objs as go
 
-df = pd.read_csv('../Datasets/CoronavirusTotal.csv')
+df = pd.read_csv('../Datasets/Olympic2016Rio.csv')
 
 df = df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
 
-df['Unrecovered'] = df['Confirmed'] - df['Deaths'] - df['Recovered']
+df['Total'] = df['Bronze'] + df['Silver'] + df['Gold']
 
-df = df[(df['Country'] != 'China')]
+new_df = df.groupby(['NOC']).agg(
+    {'Bronze': 'sum', 'Silver': 'sum', 'Gold': 'sum'}).reset_index()
 
-new_df = df.groupby(['Country']).agg(
-    {'Confirmed': 'sum', 'Deaths': 'sum', 'Recovered' : 'sum', 'Unrecovered': 'sum'}).reset_index()
+new_df = new_df.sort_values(by=['Total'], ascending=[False]).head(20).reset_index()
 
-new_df = new_df.sort_values(by=['Confirmed'], ascending=[False]).head(20).reset_index()
-
-trace1 = go.Bar(x=new_df['Country'], y=new_df['Unrecovered'], name='Unrecovered', marker={'color': '#CD7F32'})
-trace2 = go.Bar(x=new_df['Country'], y=new_df['Recovered'],name='Recovered', marker={'color': '#9EA0A1'})
-trace3= go.Bar(x=new_df['Country'], y=new_df['Deaths'], name='Deaths', marker={'color': '#FFD700'})
+trace1 = go.Bar(x=new_df['NOC'], y=new_df['Gold'], name='Gold', marker={'color': '#CD7F32'})
+trace2 = go.Bar(x=new_df['NOC'], y=new_df['Silver'],name='Silver', marker={'color': '#9EA0A1'})
+trace3= go.Bar(x=new_df['NOC'], y=new_df['Bronze'], name='Bronze', marker={'color': '#FFD700'})
 data= [trace1, trace2, trace3]
 
 layout = go.Layout(title='CoronaVirus Cases in the first 20 countries except China',
-                    xaxis_title="Country",
-                    yaxis_title="Number of Cases", barmode='stack')
+                    xaxis_title="NOC",
+                    yaxis_title="Medals", barmode='stack')
 
 fig = go.Figure(data=data, layout=layout)
 pyo.plot(fig, filename='stackbarchart.html')
