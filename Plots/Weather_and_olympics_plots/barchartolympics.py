0277bf7df87558bc56cf0490a96cd2193e031d87
atwilson0729 <atwilson0729@gmail.com>
2021-03-24 19:59:20 -0400

    Add olympic bar chart

--- Plots/Weather_and_olympics_plots/barchartolympics.py
+++ Plots/Weather_and_olympics_plots/barchartolympics.py
@@ -3,28 +3,27 @@ import plotly.offline as pyo
 import plotly.graph_objs as go
 
 # Load CSV file from Datasets folder
-df = pd.read_csv('../../Datasets/CoronavirusTotal.csv')
+df = pd.read_csv('../../Datasets/Olympic2016Rio.csv')
 
 # Filtering US Cases
-filtered_df = df[df['Country'] == 'US']
+filtered_df = df
 
-# Removing empty spaces from State column to avoid errors
+# Removing empty spaces from Countries column to avoid errors
 filtered_df = filtered_df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
 
-# Creating sum of number of cases group by State Column
-new_df = filtered_df.groupby(['State'])['Confirmed'].sum().reset_index()
+# Sorting values and select first 20 countries
+filtered_df = filtered_df.sort_values(by=['Total'], ascending=[False]).head(20)
 
-# Sorting values and select first 20 states
-new_df = new_df.sort_values(by=['Confirmed'], ascending=[False]).head(20)
 
 # Preparing data
-#loads data into a plotly graph object where the x-axis is the state and is loaded into pandas df, and the y axis is the number of confirmed cases loaded into a pandas df
-data = [go.Bar(x=new_df['State'], y=new_df['Confirmed'])]
+#loads data into a plotly graph object where the x-axis is the country and is loaded into pandas df, and the y axis is the number of total medals loaded into a pandas df
+data = [go.Bar(x=filtered_df['NOC'], y=filtered_df['Total'])]
+
 
 # Preparing layout
-layout = go.Layout(title='Corona Virus Confirmed Cases in The US', xaxis_title="States",
-                   yaxis_title="Number of confirmed cases")
+layout = go.Layout(title='2016 Olympics Medal Totals', xaxis_title="Name of Country",
+                   yaxis_title="Number of total medals")
 
 # Plot the figure and saving in a html file
 fig = go.Figure(data=data, layout=layout)
-pyo.plot(fig, filename='barchart.html')
+pyo.plot(fig, filename='barchartolympics.html')
