8f8ff7ceac47c83f9900bbf300b9d8f312978405
atwilson0729 <atwilson0729@gmail.com>
2021-03-24 18:15:23 -0400

    Uhhh

--- Plots/MultiLineChart.py
+++ Plots/MultiLineChart.py
@@ -7,12 +7,6 @@ df = pd.read_csv('../Datasets/CoronaTimeSeries.csv')
 df['Date'] = pd.to_datetime(df['Date'])
 
 # Preparing data
-# creates three plotly graph objects for the multiple lines
-# all three have x-axis as the date in a pandas df
-# First obj has y-axis of number of deaths as y axis
-# second obj has y axis of number of recovered cases
-# Third obj has a y-axis of number of unrecovered cases
-# all three graph objects are stored in a list
 trace1 = go.Scatter(x=df['Date'], y=df['Death'], mode='lines', name='Death')
 trace2 = go.Scatter(x=df['Date'], y=df['Recovered'], mode='lines', name='Recovered')
 trace3 = go.Scatter(x=df['Date'], y=df['Unrecovered'], mode='lines', name='Unrecovered')
